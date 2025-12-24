db69aacf24c67c0dffba42901b4a327a0371faa9
Thiago X99 <thiago.souzadasilva9901@gmail.com>
2025-12-24 19:09:44 -0300

    Product

---
+++ src/main/java/com/courseudemy/curso/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.courseudemy.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.courseudemy.curso.entities.Product;
+import com.courseudemy.curso.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj =repository.findById(id);
+		return obj.get();
+	}
+}
