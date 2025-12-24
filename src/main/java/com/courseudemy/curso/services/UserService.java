d81253e2765a24a9077f88d3ebcb47eac1b54efc
Thiago X99 <thiago.souzadasilva9901@gmail.com>
2025-12-23 22:06:28 -0300

    Service layer, component registration

---
+++ src/main/java/com/courseudemy/curso/services/UserService.java
@@ -0,0 +1,26 @@
+package com.courseudemy.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.courseudemy.curso.entities.User;
+import com.courseudemy.curso.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj =repository.findById(id);
+		return obj.get();
+	}
+}
