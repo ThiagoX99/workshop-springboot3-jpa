4b9ba2ec0265f8d98bd6f15c1f2ef7b86fa0bd41
Thiago X99 <thiago.souzadasilva9901@gmail.com>
2025-12-26 00:34:10 -0300

    Exception handling - update

--- src/main/java/com/courseudemy/curso/services/UserService.java
+++ src/main/java/com/courseudemy/curso/services/UserService.java
@@ -13,6 +13,8 @@ import com.courseudemy.curso.repositories.UserRepository;
 import com.courseudemy.curso.services.exceptions.DatabaseException;
 import com.courseudemy.curso.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -43,9 +45,14 @@ public class UserService {
 	}	
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id); 
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id); 
+			updateData(entity, obj);
+			return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			e.printStackTrace();
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
